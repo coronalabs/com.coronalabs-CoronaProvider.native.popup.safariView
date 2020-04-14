71b20d8dbde42b2705afa8b45129ed06092a52ee
Vlad Shcherban <vlad@coronalabs.com>
2020-04-13 23:01:07 -0400

    Adding binaries

---
+++ plugins/2014.2511/iphone-sim/metadata.lua
@@ -0,0 +1,16 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+
+		-- This is the name without the 'lib' prefix.
+		-- In this case, the static library is called: libSTATIC_LIB_NAME.a
+		staticLibs = { 'native-popup-safariView', }, 
+
+		frameworks = {},
+		frameworksOptional = {  "SafariServices"  },
+	},
+}
+
+return metadata
